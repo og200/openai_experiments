6f1017a6bc038e0cd8dce98e8ad50d9b7492fd02
Oren Goldschmidt <oren@andalso.net>
2023-03-24 10:19:48 -0400

    first build

---
+++ openai_experiments/data/msgs_account_selection.py
@@ -0,0 +1,19 @@
+X is the customer of a Park National Bank. X holds the following accounts:
+
+A checking account with a balance of $350.00. In 2022 this account was used for 24 transactions, 12 were using a debit card, 6 used bill pay, and 6 were at Park National Bank ATMs. In 2021 this account was used for 56 transactions, 44 used a debit card, 10 used bill pay, and 2 were at Park National Bank ATMs. In 2020 this account was used for 122 transactions, 98 used a debit card, 20 used Park National Bank ATMs, 4 used foreign ATMs with total feed of $22.50.
+
+A savings account with a balance of $9950.00. This account had a balance of $8200.00 in January of 2022, $7100.00 in January of 2021, and $6310.00 in January of 2020.
+
+The customer also has a mortgage with a balance of $150250.00 on a property valued at $275000.00. The mortgage carries an APR of 4.8%.
+---
+Park National Bank offers the following checking account products:
+
+1. A Premium Account: this costs $10 per month, pays 1% interest, and covers all fees on up to 10 foreign ATM transactions. The fees on this account are zero for customers who use more than 20 debit card transactions in a year.
+
+2. A Standard Account: this costs $5 per month, pays 0.2% interest. It does not cover any fees on foreign ATM transactions. The fees on this account are zero for customers who use more than 10 debit card transactions in a year.
+
+3. A free account: this is free (no cost), pays no interest, and does not cover any foreign ATM transactions.
+
+
+
+Which of Park National Bank's checking account products would best suit customer X?
