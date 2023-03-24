ec45f9687b9d72fb2f608796576c2ac35d0bb73f
Oren Goldschmidt <oren@andalso.net>
2023-03-24 07:38:04 -0700

    with code this time

---
+++ openai_experiments/sketches/basic.py
@@ -0,0 +1,19 @@
+import openai
+
+from openai_experiments import config
+from openai_experiments.data import msgs_account_selection
+
+openai.api_key = config.api_key
+
+
+def print_models():
+    models = openai.Model.list()
+    print(models)
+
+
+if __name__ == '__main__':
+    completion = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages = msgs_account_selection.messages
+    )
+    print(completion.choices[0].message.content)
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup, find_packages
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join(path, filename))
+    return paths
+
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+long_description = f.read()
+f.close()
+
+setup(
+    name='openai_experiments',
+    version='0.0.1',
+    description='Sketches for OpenAI experimentation',
+    long_description=long_description,
+    author='Flow Innovation',
+    author_email='info@flow-innovation.com',
+    url='http://www.github.io/og200/openai_experiments/',
+    packages=find_packages(),
+    package_data={
+        '': [],
+    },
+    install_requires=[
+        'openai'
+    ]
+)
